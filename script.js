2059601148d1c1fc0312c611f3501dc1e94b4b1c
Justin Thompson <devjusty@gmail.com>
2023-05-16 16:36:08 -0400

    improved output and error handling

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const keywordEl = document.getElementById('keywords');
-const lengthEl = document.getElementById('length');
-const formEl = document.getElementById('form');
-const ipsumEl = document.getElementById('ipsum');
+const keywordElement = document.getElementById("keywords");
+const lengthElement = document.getElementById("length");
+const formElement = document.getElementById("form");
+const ipsumElement = document.getElementById("ipsum");
+const outputElement = document.getElementById("output");
 
-let keywords = keywordEl.value
-
-function loremIpsumGenerator(keywords, length) {
+function generateIpsum(keywords, length) {
   const paragraphCount = length;
-  const words = [
-    "lorem",
+  // Define an array of Lorem Ipsum words
+  const loremIpsumWords = [
+    "Lorem",
     "ipsum",
     "dolor",
     "sit",
@@ -26,80 +26,130 @@ function loremIpsumGenerator(keywords, length) {
     "et",
     "dolore",
     "magna",
-    "aliqua"
+    "aliqua",
+    "Ut",
+    "enim",
+    "ad",
+    "minim",
+    "veniam",
+    "quis",
+    "nostrud",
+    "exercitation",
+    "ullamco",
+    "laboris",
+    "nisi",
+    "ut",
+    "aliquip",
+    "ex",
+    "ea",
+    "commodo",
+    "consequat",
+    "Duis",
+    "aute",
+    "irure",
+    "dolor",
+    "in",
+    "reprehenderit",
+    "in",
+    "voluptate",
+    "velit",
+    "esse",
+    "cillum",
+    "dolore",
+    "eu",
+    "fugiat",
+    "nulla",
+    "pariatur",
+    "Excepteur",
+    "sint",
+    "occaecat",
+    "cupidatat",
+    "non",
+    "proident",
+    "sunt",
+    "in",
+    "culpa",
+    "qui",
+    "officia",
+    "deserunt",
+    "mollit",
+    "anim",
+    "id",
+    "est",
+    "laborum",
   ];
-  const sentences = [
-    "Lorem ipsum dolor sit amet.",
-    "Consectetur adipiscing elit.",
-    "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "Ut enim ad minim veniam.",
-    "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    "Excepteur sint occaecat cupidatat non proident.",
-    "Sunt in culpa qui officia deserunt mollit anim id est laborum."
-  ];
-  let paragraphs = "";
-  for (let i = 0; i < paragraphCount; i++) {
-    let sentenceCount = Math.floor(Math.random() * 5) + 1;
-    let paragraph = "";
-    for (let j = 0; j < sentenceCount; j++) {
-      let sentence = sentences[Math.floor(Math.random() * sentences.length)];
-      let wordsCount = Math.floor(Math.random() * (length / sentenceCount)) + 1;
-      let sentenceWords = [];
-      for (let k = 0; k < wordsCount; k++) {
-        let word = words[Math.floor(Math.random() * words.length)];
-        if (k === 0) {
-          word = word.charAt(0).toUpperCase() + word.slice(1);
-        }
-        sentenceWords.push(word);
+
+  const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  // Generate a random sentence using the keywords
+  const randomSentence = () => {
+    const sentence = [];
+    let wordCount = getRandomInt(3, 15);
+    for (let i = 0; i < wordCount; i++) {
+      if (Math.random() > 0.7 && keywords.length > 0) {
+        sentence.push(keywords[Math.floor(Math.random() * keywords.length)]);
+      } else {
+        sentence.push(
+          loremIpsumWords[Math.floor(Math.random() * loremIpsumWords.length)]
+        );
       }
-      sentence += " " + sentenceWords.join(" ");
-      paragraph += sentence + " ";
     }
-    paragraphs += "<p>" + paragraph + "</p>";
-  }
-  return paragraphs;
+    // Capitalize the first letter of the first word in the sentence
+    const capitalizedSentence =
+      sentence.join(" ").charAt(0).toUpperCase() + sentence.join(" ").slice(1);
+    return capitalizedSentence + ".";
+  };
+
+  // Generate a random paragraph using the random sentence generator
+  const getRandomParagraph = () => {
+    const paragraph = [];
+    let sentenceCount = getRandomInt(1, 7);
+    for (let i = 0; i < sentenceCount; i++) {
+      paragraph.push(randomSentence(sentenceCount));
+    }
+    return paragraph.join(" ");
+  };
+
+  // Generate the final Lorem Ipsum text using the random paragraph generator
+  const generateLoremIpsumText = () => {
+    const text = [];
+    for (let i = 0; i < paragraphCount; i++) {
+      text.push(getRandomParagraph());
+    }
+    return text.join("\n\n");
+  };
+  // Return the final Lorem Ipsum text
+  return generateLoremIpsumText();
 }
 
-
-formEl.addEventListener("submit", (e) => {
+formElement.addEventListener("submit", (e) => {
   e.preventDefault();
+  console.log(keywordElement.value);
+const keywords = keywordElement.value.split(/[,\s]+/);
+  const length = lengthElement.value;
+  console.log(keywords);
+  if (!keywords.value || isNaN(length) || length <= 0) {
+    outputElement.innerText = "enter some keywords and a valid number";
+    ipsumElement.innerText = "";
+  } else {
+    outputElement.innerText = "";
+    ipsumElement.innerText = generateIpsum(keywords, length);
+  }
+});
 
-  const keywords = keywordEl.value.split(', ');
-  console.log(`keywords: ${keywords}`);
-  const length = lengthEl.value
-  console.log(`length: ${length}`);
+const copyButtonEl = document.getElementById("copy");
 
-  console.log(ipsumEl);
-
-  ipsumEl.value = loremIpsumGenerator(keywords, length)
-
-})
-
-const copyButtonEl = document.getElementById('copy')
-
-copyButtonEl.addEventListener('click', () =>{
-
-  navigator.clipboard.writeText(ipsumEl.value)
-    .then(() => {
-      console.log('copied to clipboard')
+copyButtonEl.addEventListener("click", () => {
+  navigator.clipboard.writeText(ipsumElement.value).then(
+    () => {
+      outputElement.innerText = "copied to clipboard";
+      console.log("copied to clipboard");
     },
     () => {
-      console.log('not copied to clipboard')
-    })
-
-})
-
-
-// copyButtonEl.onclick(() => {
-
-//     navigator.clipboard.writeText(this.ipsumEl.value)
-//     .then(() => {
-//       this.copiedCode.innerHTML = '<div class="alert">Copied to clipboard \u{1F44D}</div>'
-//     }, () => {
-//       this.copiedCode.innerHTML = '<div class="alert">\u{1F494} Not Supported</div>'
-//     }), setTimeout(() => {
-//       this.copiedCode.innerHTML = ""
-//     }, 2e3)
-
-// })
+      outputElement.innerText = "unable to copy for some reason";
+      console.log("not copied to clipboard");
+    }
+  );
+});
