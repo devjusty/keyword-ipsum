db9690eb3aeb2079ae0f170a772daaabde09fefb
Justin Thompson <devjusty@gmail.com>
2023-06-22 00:11:13 -0400

    styling and package updates

--- keyword-ipsum/src/App.jsx
+++ keyword-ipsum/src/App.jsx
@@ -1,20 +1,26 @@
 import bookLogo from "/bx-book-open.svg";
 import "./App.css";
-import Generator from './components/Generator';
+import Generator from "./components/Generator";
 
 function App() {
-
   return (
     <>
       <div className="container mx-auto">
-        <img src={bookLogo} className="logo mx-auto" alt="Vite logo" />
-        <h1 className='py-2'>Keyword Ipsum</h1>
-        <p className='text-lg lg:w-2/3 mx-auto sm:w-auto'>
+        <div className="navbar">
+          <img
+            src={bookLogo}
+            className="navbar-start w-16 h-16"
+            alt="Keyword Ipsum logo"
+          />
+          <div className="flex-none">
+          <h1 className="p-1 text-xl text-center">Keyword Ipsum</h1>
+          </div>
+        </div>
+
+        <p className="text-lg lg:w-2/3 mx-auto sm:w-auto">
           Use this Lorem Ipsum Generator to generate custom text with your own
           keywords. Enter your list of keywords and sentence length, and
-          generate unique Lorem Ipsum text for your project or design. Perfect
-          for filling in placeholder text and visualizing design layouts. Try it
-          now!
+          generate unique Lorem Ipsum text for your project or design.
         </p>
         <Generator />
       </div>
--- keyword-ipsum/src/components/Generator.jsx
+++ keyword-ipsum/src/components/Generator.jsx
@@ -29,75 +29,85 @@ const Generator = () => {
   return (
     <div>
       <form
-        className="form flex flex-row items-center justify-center gap-2 py-2 mx-auto"
+        className="form py-4 mx-auto"
         onSubmit={handleSubmit}
       >
-        <div className="form-control max-w-xs text-black">
-          <label className="label" htmlFor="keywords">
-            <span className="label-text">Keywords</span>
-          </label>
-          <input
-            type="text"
-            placeholder="awesome, radical, sick"
-            value={keywords}
-            id="keywords"
-            onChange={(e) => setKeywords(e.target.value)}
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-        <div className="form-control max-w-xs">
-          <label className="label" htmlFor="length">
-            <span className="label-text">Length</span>
-          </label>
-          <input
-            type="number"
-            id="length"
-            min={1}
-            value={length}
-            onChange={(e) => setLength(e.target.value)}
-            className="input input-bordered w-full max-w-xs text-black"
-          />
-        </div>
-        <div className="form-control max-w-xs">
-          {" "}
-          <label htmlFor="unit" className="label">
-            <span className="label-text">Units</span>
-          </label>
-          <div className="join">
+        <div className="flex flex-row align-items justify-center py-2 gap-2">
+          <div className="form-control max-w-xs text-black">
+            <label className="label" htmlFor="keywords">
+              <span className="label-text text-primary">Keywords</span>
+            </label>
             <input
-              className="join-item btn"
-              type="radio"
-              value="words"
-              checked={unit === "words"}
-              name="options"
-              aria-label="Words"
-              onChange={handleUnitChange}
+              type="text"
+              placeholder="awesome, radical, sick"
+              value={keywords}
+              id="keywords"
+              onChange={(e) => setKeywords(e.target.value)}
+              className="input input-bordered w-full max-w-xs"
             />
+          </div>
+          <div className="form-control max-w-xs">
+            <label className="label" htmlFor="length">
+              <span className="label-text text-primary">Length</span>
+            </label>
             <input
-              className="join-item btn"
-              type="radio"
-              value="sentences"
-              checked={unit === "sentences"}
-              name="options"
-              aria-label="Sentences"
-              onChange={handleUnitChange}
+              type="number"
+              id="length"
+              min={1}
+              value={length}
+              onChange={(e) => setLength(e.target.value)}
+              className="input input-bordered w-full max-w-xs text-black"
             />
-            <input
-              className="join-item btn"
-              type="radio"
-              value="paragraphs"
-              checked={unit === "paragraphs"}
-              name="options"
-              aria-label="Paragraphs"
-              onChange={handleUnitChange}
-            />
+          </div>
+          <div className="form-control max-w-xs">
+            <label htmlFor="units" className="label">
+              <span className="label-text text-primary">Units</span>
+            </label>
+            <div className="join">
+              <input
+                className="join-item btn"
+                type="radio"
+                value="words"
+                checked={unit === "words"}
+                name="unites"
+                aria-label="Words"
+                onChange={handleUnitChange}
+              />
+              <input
+                className="join-item btn"
+                type="radio"
+                value="sentences"
+                checked={unit === "sentences"}
+                name="units"
+                aria-label="Sentences"
+                onChange={handleUnitChange}
+              />
+              <input
+                className="join-item btn"
+                type="radio"
+                value="paragraphs"
+                checked={unit === "paragraphs"}
+                name="units"
+                aria-label="Paragraphs"
+                onChange={handleUnitChange}
+              />
+            </div>
           </div>
         </div>
 
-        <div className="form-control max-w-xs self-end">
-          <button type="submit" className="btn btn-primary">
-            Make My Ipsum
-          </button>
+        <div className="flex flex-row align-items justify-center py-2 gap-2">
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text text-primary">Add Synonyms</span>
+              <input type="checkbox" className="toggle" checked />
+            </label>
+          </div>
+
+          <div className="form-control max-w-xs self-end">
+            <button type="submit" className="btn btn-primary">
+              Make My Ipsum
+            </button>
+          </div>
         </div>
       </form>
       <div className="form-control w-full">
@@ -106,7 +116,7 @@ const Generator = () => {
           placeholder={ipsumText}
         ></textarea>
       </div>
-      <button className="btn btn-secondary">Copy My Ipsum</button>
+      <button className="btn btn-secondary my-2">Copy My Ipsum</button>
       {error && <p>{error}</p>}
       {ipsumText && <p>{ipsumText}</p>}
     </div>
