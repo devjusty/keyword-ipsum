88677ef497c6f243c1d28e41398ba970ff627a4d
Justin Thompson <devjusty@gmail.com>
2025-03-27 12:00:11 -0400

    Update Analytics, Add Canonical URL

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ReactGA from "react-ga4";
 import './index.css'
 import "./assets/fonts/RubikScribble-Regular.woff2";
+
+ReactGA.initialize("G-X3MZ25KN64");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
